c33262cbe6e6b1b72ba7b4bc3e7ab2386785c184
XiaoBing <2294175669@qq.com>
2021-05-25 22:47:23 +0800

    HK week12 Commit

---
+++ src/main/java/com/XiaoBing/controller/ProductDetailsServlet.java
@@ -0,0 +1,97 @@
+package com.XiaoBing.controller;
+
+import com.XiaoBing.dao.ProductDao;
+
+import com.XiaoBing.model.Category;
+
+import com.XiaoBing.model.Product;
+
+
+
+import javax.servlet.*;
+
+import javax.servlet.http.*;
+
+import javax.servlet.annotation.*;
+
+import java.io.IOException;
+
+import java.sql.Connection;
+
+import java.sql.SQLException;
+
+import java.util.List;
+
+
+
+@WebServlet(name = "ProductDetailsServlet", value = "/productDetails")
+
+public class ProductDetailsServlet extends HttpServlet {
+
+    Connection con=null;
+
+
+
+    @Override
+
+    public void init() throws ServletException {
+
+        super.init();;
+
+        con=(Connection) getServletContext().getAttribute("con");
+
+    }
+
+    @Override
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        try {
+
+            List<Category> categoryList=Category.findallCategory(con);
+
+            request.setAttribute("categoryLsit",categoryList);
+
+        } catch (SQLException throwables) {
+
+            throwables.printStackTrace();
+
+        }
+
+        try {
+
+            if(request.getParameter("id")!=null) {
+
+                int productId = Integer.parseInt(request.getParameter("id"));
+
+                ProductDao productDao = new ProductDao();
+
+                Product product = productDao.findById(productId, con);
+
+                request.setAttribute("p",product);
+
+            }
+
+        } catch (SQLException throwables) {
+
+            throwables.printStackTrace();
+
+        }
+
+        String path="/WEB-INF/views/productDetails.jsp";
+
+        request.getRequestDispatcher(path).forward(request,response);
+
+    }
+
+
+
+    @Override
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+
+
+    }
+
+}
---
+++ src/main/java/com/XiaoBing/controller/ShopServlet.java
@@ -0,0 +1,105 @@
+package com.XiaoBing.controller;
+
+import com.XiaoBing.dao.ProductDao;
+
+import com.XiaoBing.model.Category;
+
+import com.XiaoBing.model.Product;
+
+
+
+import javax.servlet.*;
+
+import javax.servlet.http.*;
+
+import javax.servlet.annotation.*;
+
+import java.io.IOException;
+
+import java.sql.Connection;
+
+import java.sql.SQLException;
+
+import java.util.List;
+
+
+
+@WebServlet(name = "ShopServlet", value = "/shop")
+
+public class ShopServlet extends HttpServlet {
+
+    Connection con=null;
+
+
+
+    @Override
+
+    public void init() throws ServletException {
+
+        super.init();;
+
+        con=(Connection) getServletContext().getAttribute("con");
+
+    }
+
+    @Override
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        Category category=new Category();
+
+        try {
+
+            List<Category> categoryList=category.findallCategory(con);
+
+            request.setAttribute("categoryList",categoryList);
+
+        } catch (SQLException throwables) {
+
+            throwables.printStackTrace();
+
+        }
+
+        ProductDao productDao=new ProductDao();
+
+        try {
+
+            if(request.getParameter("categoryId")==null) {
+
+                List<Product> productList = productDao.findAll(con);
+
+                request.setAttribute("productList", productList);
+
+            }else{
+
+                int catId=Integer.parseInt(request.getParameter("categoryId"));
+
+                List<Product> productList =productDao.findByCategoryId(catId,con);
+
+                request.setAttribute("productList", productList);
+
+            }
+
+        } catch (SQLException throwables) {
+
+            throwables.printStackTrace();
+
+        }
+
+        String path="/WEB-INF/views/shop.jsp";
+
+        request.getRequestDispatcher(path).forward(request,response);
+
+    }
+
+
+
+    @Override
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+
+
+    }
+
+}
